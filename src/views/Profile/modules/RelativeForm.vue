3d65b051b97ef453ff042f3c6870b1d7ef87df41
kazzrom <roma.kazansew.04@mail.ru>
2024-04-07 14:21:42 +0500

    Сверстан раздел с семьёй

---
+++ src/views/Profile/modules/RelativeForm.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(["relative"]);
+
+const relative = ref(props.relative);
+</script>
+
+<template>
+  <div class="form_wrapper">
+    <h4 class="font-bold mb-5">{{ relative.kinship }}</h4>
+    <div class="form">
+      <div class="input-text">
+        <label for="surname">Фамилия</label>
+        <InputText id="surname" v-model="relative.surname" />
+      </div>
+      <div class="input-text">
+        <label for="name">Имя</label>
+        <InputText id="name" v-model="relative.name" />
+      </div>
+      <div class="input-text">
+        <label for="patronymic">Отчество</label>
+        <InputText id="patronymic" v-model="relative.patronymic" />
+      </div>
+      <div class="input-text">
+        <label for="phoneNumber">Номер телефона</label>
+        <InputText id="phoneNumber" v-model="relative.phoneNumber" />
+      </div>
+      <div class="input-text">
+        <label for="phoneNumber">Место работы</label>
+        <InputText id="phoneNumber" v-model="relative.workPlace" />
+      </div>
+      <div class="input-text">
+        <label for="phoneNumber">Должность</label>
+        <InputText id="phoneNumber" v-model="relative.jobTitle" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.form_wrapper {
+  @apply flex flex-col;
+}
+
+.form {
+  @apply grid grid-cols-3 gap-5;
+}
+
+.input-text {
+  @apply flex flex-col gap-2 w-[260px];
+}
+
+.input-text label {
+  @apply font-bold;
+}
+</style>
--- src/views/Profile/sections/FamilySection.vue
+++ src/views/Profile/sections/FamilySection.vue
@@ -1,4 +1,38 @@
+<script setup>
+import RelativeForm from "../modules/RelativeForm.vue";
+const relatives = [
+  {
+    name: "Фёдор",
+    surname: "Молчанов",
+    patronymic: "Глебович",
+    phoneNumber: "+79122717256",
+    workPlace: "Кухня",
+    jobTitle: "Повар",
+    kinship: "Отец",
+  },
+  {
+    name: "Ясмина",
+    surname: "Молчанова",
+    patronymic: "Тимуровна",
+    phoneNumber: "+79122717256",
+    workPlace: "Кухня",
+    jobTitle: "Повар",
+    kinship: "Мать",
+  },
+];
+</script>
 <template>
-  <h1>Семья</h1>
+  <div class="wrapper">
+    <RelativeForm
+      v-for="(relative, index) in relatives"
+      :key="index"
+      :relative="relative"
+    />
+  </div>
 </template>
-    
+
+<style scoped>
+.wrapper {
+  @apply flex flex-col gap-14 pl-14 pt-10;
+}
+</style>
